6a4dadfd991e5dbf91d01d5239f067f4cc964ca6
tatiana <tenb_@mail.ru>
2023-01-31 22:31:23 +0700

    Семинар 6

---
+++ Task40/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40: 
+// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите длину первой стороны");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны");
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+bool CompareSide(int arg1, int arg2, int arg3)
+{
+    return arg1 < arg2 + arg3 && arg2 < arg1 + arg3 && arg3 < arg2 + arg1;
+}
+
+bool result = CompareSide(sideA, sideB, sideC);
+Console.WriteLine(result ? "Да" : "Нет");
---
+++ Task42/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 42: 
+// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int BinarNumber(int num)
+{
+    int res = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        res += num % 2 * count;
+        num /= 2;
+        count *= 10;
+    }
+    return res;
+}
+
+int result = BinarNumber(number);
+Console.WriteLine(result);
+
+
+// решение 2
+
+string DecToBin(int num)
+{
+string result = string.Empty;
+while (num > 0)
+{
+result = num % 2 + result;
+num = num /2;
+}
+
+return result;
+}
+
+
+Console.Write("Введите десятичное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+string decToBin = DecToBin(number1);
+
+Console.Write(number1);
+Console.Write(" -> ");
+Console.Write(decToBin);
