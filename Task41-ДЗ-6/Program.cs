6292eac8e3e9e2d591ad40d34e4b47053e8fa578
tatiana <tenb_@mail.ru>
2023-02-04 23:29:33 +0700

    домашнее задание 6

---
+++ Task41-ДЗ-6/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+}
+
+int[] CreateArrayBySize(int size)
+{
+    int[] array = new int[size];
+    Console.WriteLine("Вводите числа по одному");
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int GetCountPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите количество проверяемых чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArrayBySize(m);
+int countPositive = GetCountPositive(arr);
+PrintArray(arr);
+Console.Write(" -> " + countPositive);
---
+++ Task43-ДЗ-6/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] IntersectionPoint(double k1, double k2, double b1, double b2)
+{
+    double[] result=new double[2];
+    result[0] = (b2-b1)/(k1-k2);
+    result[1] = k1*result[0]+b1;
+    return result;
+}
+
+Console.WriteLine("Введите коэффициент b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Заданные порямые паралельны!");
+    return;
+}
+
+double[] res = IntersectionPoint(k1, k2, b1, b2); 
+Console.WriteLine(b1 + ", " + k1 + ", " + b2 + ", " + k2);
+double x = Math.Round(res[0], 2);
+double y = Math.Round(res[1], 2);
+Console.WriteLine("Точка пересечкния" + " -> " + x + ", " + y); 
